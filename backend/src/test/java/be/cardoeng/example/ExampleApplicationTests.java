fe8c9a426500397394da07e315a667140f3db5d9
Guillaume Cardoen <G.Cardoen@outlook.com>
2023-09-05 13:36:34 +0200

    initial commit

---
+++ backend/src/test/java/be/cardoeng/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package be.cardoeng.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
